6912395e95e00d25f50e5653db473d8d435448bb
ronjonxu <467177324@qq.com>
2024-04-23 20:00:55 +0800

    init

--- test.sh
+++ test.sh
@@ -11,6 +11,6 @@ python test.py \
     --dataset_name  RealNAID    --model nirnaf   --name $name         --dataroot $dataroot  \
     --load_iter    1    --save_imgs True   --calc_metrics True  --gpu_id $device   --mask_size 1  
 
-python 3metrics_rgbnir.py  --name $name --dataroot $dataroot --device $device  
-python metrics_rgbnir.py  --name $name --dataroot $dataroot --device $device  
+python metrics3.py  --name $name --dataroot $dataroot --device $device  # test images of three different noise levels independently
+python metrics.py  --name $name --dataroot $dataroot --device $device   # test images of three different noise levels together
  
